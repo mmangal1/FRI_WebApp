7c94846327fae76bbee23555747cfa3cf9d51b2a
Maitri Mangal <mmmangal1@binghamton.edu>
2017-04-04 00:19:46 -0400

    can click on icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,12 @@
  * Created by maitri on 3/16/2017.
  */
 jQuery(document).ready(function($){
-    $("#icon1").click(function(){
-         window.open('game.html');
-    });
-
+    var i = 1;
+    var icon_names = {'icon1':'game.html', 'icon2':'book.html', 'icon3':'calender.html', 'icon4':'news.html',
+                'icon5':'calculator.html', 'icon6':'settings.html', 'icon7':'video.html', 'icon8':'tools.html'};
+    for(i; i < 9; i++){
+        $("#icon"+i).click(function(e){
+         window.open(icon_names[e.target.id]);
+        });
+    }
 });
